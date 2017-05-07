2d659c854334c9a9f75dc51f161a3e6b62401d8e
lienhua34 <lienhua34@163.com>
2017-05-08 01:08:58 +0800

    turn down the save_checkpoints_secs option.

--- mnist/train_model.sh
+++ mnist/train_model.sh
@@ -17,6 +17,6 @@
 
 export TF_MAX_STEPS=3000
 export TF_LOGDIR=/tmp/mnist-log1
-export TF_SAVE_CHECKPOINTS_SECS=60
+export TF_SAVE_CHECKPOINTS_SECS=2
 export TF_SAVE_SUMMARIES_STEPS=10
 python taas_mnist.py --data_dir=/tmp/mnist-data
